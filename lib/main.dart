07d655316a2be869a514e1662135b3c36dc342df
Sewjith <sewjithsilva@users.noreply.github.com>
2025-01-19 08:24:32 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 200,
+            color: const Color.fromARGB(255, 118, 218, 232),
+            child: Center(child: Text("Main Branch!"),),
+          ),
+        ),
+      ),
+    );
+    
+  }
+}
