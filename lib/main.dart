66b544ec0ab15c4bfec2f81c0be07c992d0fd885
Sewjith <sewjithsilva@users.noreply.github.com>
2025-02-02 15:37:46 +0530

    Navigation bar updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,59 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0; // To track the current selected index
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(title: Text('Demo!')),
         body: Center(
           child: Container(
             height: 200,
             color: const Color.fromARGB(255, 118, 218, 232),
-            child: Center(child: Text("Demo!"),),
+            child: Center(child: Text("Demo!")),
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex, // Current selected index
+          onTap: _onItemTapped, // Handle item tap
+          selectedItemColor: Colors.blue, // Change color when selected
+          unselectedItemColor: Colors.grey[700], // Change color when unselected
+          type: BottomNavigationBarType.fixed, // Fixed navigation bar, no animation
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "", // Empty label (icon only)
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: "", // Empty label (icon only)
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "", // Empty label (icon only)
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: "", // Empty label (icon only)
+            ),
+          ],
         ),
       ),
     );
-    
   }
 }
