4f9b5915545416009c3685724b532871e9586fee
Sewjith <sewjithsilva@users.noreply.github.com>
2025-02-16 19:04:19 +0530

    nav bar updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'bottom_nav_bar.dart'; 
 
 void main() {
   runApp(MyApp());
@@ -24,37 +25,10 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('Demo!')),
-        body: Center(
-          child: Container(
-            height: 200,
-            color: const Color.fromARGB(255, 118, 218, 232),
-            child: Center(child: Text("Demo!")),
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedIndex, // Current selected index
-          onTap: _onItemTapped, // Handle item tap
-          selectedItemColor: Colors.blue, // Change color when selected
-          unselectedItemColor: Colors.grey[700], // Change color when unselected
-          type: BottomNavigationBarType.fixed, // Fixed navigation bar, no animation
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "", // Empty label (icon only)
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              label: "", // Empty label (icon only)
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "", // Empty label (icon only)
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today),
-              label: "", // Empty label (icon only)
-            ),
-          ],
+        
+        bottomNavigationBar: BottomNavBar(
+          selectedIndex: _selectedIndex, // Pass selected index
+          onItemTapped: _onItemTapped, // Pass onTap callback
         ),
       ),
     );
