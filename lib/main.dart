c7f7e4dc39cb12a2d3450a0dd153372c9889e460
Thimira-Hansana <148532453+Thimira-Hansana@users.noreply.github.com>
2025-03-04 10:42:39 +0530

    Update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'appointment_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Doctor Appointment Management',
+      title: 'Appointment Schedule',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
+        primaryColor: const Color(0xFF2260FF),
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF2260FF),
+        ),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          bodyLarge: TextStyle(fontSize: 14),
+        ),
       ),
       home: const AppointmentManagementPage(),
     );
@@ -41,47 +49,44 @@ class _AppointmentManagementPageState extends State<AppointmentManagementPage> {
       'age': '30',
       'description': 'Headache',
       'time': '15:00',
-      'date': '2025-02-21'
+      'date': '2025-03-04'
     },
     {
       'name': 'Jane Smith',
       'age': '25',
       'description': 'Fever',
       'time': '16:30',
-      'date': '2025-02-21'
+      'date': '2025-03-04'
     },
     {
       'name': 'Emma Johnson',
       'age': '28',
       'description': 'Coughing',
       'time': '17:00',
-      'date': '2025-02-22'
+      'date': '2025-03-05'
     },
     {
       'name': 'David Lee',
       'age': '35',
       'description': 'Back Pain',
       'time': '18:00',
-      'date': '2025-02-23'
+      'date': '2025-03-06'
     },
     {
       'name': 'Sarah Brown',
       'age': '40',
       'description': 'Stomach Ache',
       'time': '19:00',
-      'date': '2025-02-24'
+      'date': '2025-03-06'
     },
     {
       'name': 'Michael Davis',
       'age': '50',
       'description': 'High Blood Pressure',
       'time': '20:00',
-      'date': '2025-02-24'
+      'date': '2025-03-06'
     },
   ];
-
-  DateTime _startDate = DateTime.now();
-  DateTime _endDate = DateTime.now();
 
   @override
   void initState() {
@@ -97,141 +102,172 @@ class _AppointmentManagementPageState extends State<AppointmentManagementPage> {
     super.dispose();
   }
 
-  List<Map<String, String>> getFilteredAppointments() {
+  List<Map<String, String>> getFilteredAppointments(DateTime selectedDate) {
     return appointments.where((appointment) {
       DateTime appointmentDate =
           DateFormat('yyyy-MM-dd').parse(appointment['date']!);
-      return appointmentDate.isAtSameMomentAs(_selectedDate);
+      return appointmentDate.isAtSameMomentAs(selectedDate);
     }).toList();
   }
 
-  String _getRemainingTime(String appointmentTime) {
+  String _getRemainingTime(String appointmentTime, String appointmentDate) {
     DateTime now = DateTime.now();
-    DateTime appointmentDateTime = DateFormat('HH:mm').parse(appointmentTime);
-    appointmentDateTime = DateTime(now.year, now.month, now.day,
-        appointmentDateTime.hour, appointmentDateTime.minute);
+
+    DateTime appointmentDateTime = DateFormat('yyyy-MM-dd HH:mm')
+        .parse('$appointmentDate $appointmentTime');
 
     if (appointmentDateTime.isBefore(now)) {
       return "Started";
     }
 
     Duration remaining = appointmentDateTime.difference(now);
-    int minutes = remaining.inMinutes;
 
-    // Check if it's within 10 minutes before the appointment
-    if (minutes == 10) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        _showTimeAlertDialog(context, appointmentTime);
-      });
-    }
-
-    int hours = remaining.inHours;
-    minutes = remaining.inMinutes % 60;
+    int days = remaining.inDays;
+    int hours = remaining.inHours % 24;
+    int minutes = remaining.inMinutes % 60;
     int seconds = remaining.inSeconds % 60;
 
-    return "$hours h $minutes m $seconds s remaining";
+    return "${days.toString().padLeft(2, '0')}:${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
   }
 
-  void _showTimeAlertDialog(BuildContext context, String appointmentTime) {
+  void _showStartStreamConfirmationDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: const Text("10 Minutes Left!"),
-        content: Text(
-            "The appointment at $appointmentTime is about to start in 10 minutes."),
+        title: const Text("Start Stream"),
+        content: const Text(
+            "Are you sure you want to start the stream for this appointment?"),
         actions: [
           TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              // Handle stream start logic here
+            },
+            child: const Text("Yes"),
+          ),
+          TextButton(
             onPressed: () => Navigator.pop(context),
-            child: const Text("OK"),
+            child: const Text("No"),
           ),
         ],
       ),
     );
   }
 
-  void _showDateRangePicker(BuildContext context) async {
-    final DateTimeRange? picked = await showDateRangePicker(
+  void _showAppointmentDetailsDialog(
+      BuildContext context, Map<String, String> appointment) {
+    showDialog(
       context: context,
-      initialDateRange: DateTimeRange(start: _startDate, end: _endDate),
-      firstDate: DateTime.now().subtract(const Duration(days: 365)),
-      lastDate: DateTime.now().add(const Duration(days: 365)),
-      builder: (BuildContext context, Widget? child) {
-        return Theme(
-          data: ThemeData.light().copyWith(
-            primaryColor: Colors.blue,
-            buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary),
+      barrierDismissible: true,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
-          child: child!,
+          backgroundColor: Colors.transparent,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                  child: Container(
+                    color: Colors.black.withOpacity(0),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  width: 300,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Patient: ${appointment['name']}',
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Age: ${appointment['age']} | Description: ${appointment['description']}',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Appointment Time: ${appointment['time']}',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          _showStartStreamConfirmationDialog(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF2260FF),
+                        ),
+                        child: const Text(
+                          'Start Consultation Stream',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 8,
+                left: 8,
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Colors.white),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ],
+          ),
         );
       },
-    );
-
-    if (picked != null) {
-      setState(() {
-        _startDate = picked.start;
-        _endDate = picked.end;
-      });
-    }
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => AppointmentViewPage(
-          startDate: _startDate,
-          endDate: _endDate,
-          appointments: appointments,
-        ),
-      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, String>> filteredAppointments = getFilteredAppointments();
+    List<Map<String, String>> filteredAppointments =
+        getFilteredAppointments(_selectedDate);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Appointment Management'),
+        title: const Text('Appointment Schedule'),
+        backgroundColor: const Color(0xFF2260FF),
+        elevation: 0,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Date Range Selection Row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Select Date Range:',
-                    style: Theme.of(context).textTheme.bodyLarge),
-                IconButton(
-                  icon: const Icon(Icons.filter_list, color: Colors.blue),
-                  onPressed: () => _showDateRangePicker(context),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-
-            // Date Picker
             CalendarDatePicker(
               initialDate: _selectedDate,
               firstDate: DateTime.now().subtract(const Duration(days: 365)),
               lastDate: DateTime.now().add(const Duration(days: 365)),
               onDateChanged: (date) {
                 setState(() {
-                  _selectedDate = date; // Update the selected date
+                  _selectedDate = date;
                 });
               },
             ),
             const SizedBox(height: 16),
-
-            // Display Appointments for Selected Date
             Text(
               'Appointments for ${DateFormat('yyyy-MM-dd').format(_selectedDate)}',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                    color: Colors.black,
+                  ),
             ),
             const SizedBox(height: 10),
-
-            // List of Filtered Appointments
             Expanded(
               child: filteredAppointments.isNotEmpty
                   ? ListView.builder(
@@ -239,8 +275,13 @@ class _AppointmentManagementPageState extends State<AppointmentManagementPage> {
                       itemBuilder: (context, index) {
                         var appointment = filteredAppointments[index];
                         return Card(
-                          color: const Color(0xFFCAD6FF),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          elevation: 5,
+                          margin: const EdgeInsets.symmetric(vertical: 8),
+                          color: Colors.lightBlue[50],
                           child: ListTile(
+                            contentPadding: const EdgeInsets.all(12),
                             leading: CircleAvatar(
                               backgroundColor: const Color(0xFF2260FF),
                               child: Text(
@@ -250,11 +291,17 @@ class _AppointmentManagementPageState extends State<AppointmentManagementPage> {
                             ),
                             title: Text(appointment['name']!),
                             subtitle: Text(
-                              '${appointment['age']} years old\n${appointment['description']}\nRemaining: ${_getRemainingTime(appointment['time']!)}',
+                              'Age: ${appointment['age']} | ${appointment['description']}\nTime: ${appointment['time']} | ${_getRemainingTime(appointment['time']!, appointment['date']!)}',
                               style: const TextStyle(fontSize: 14),
                             ),
+                            trailing: const Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20,
+                              color: Colors.grey,
+                            ),
                             onTap: () {
-                              _showStartStreamDialog(context);
+                              _showAppointmentDetailsDialog(
+                                  context, appointment);
                             },
                           ),
                         );
@@ -262,41 +309,13 @@ class _AppointmentManagementPageState extends State<AppointmentManagementPage> {
                     )
                   : const Center(
                       child: Text(
-                        'No Appointments for Selected Date',
+                        'No Appointments for This Day',
                         style: TextStyle(fontSize: 18, color: Colors.grey),
                       ),
                     ),
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  void _showStartStreamDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text("Start Streaming"),
-        content: const Text("Do you want to start the stream?"),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text("Cancel"),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFF2260FF),
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text(
-              "Start Stream",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ],
       ),
     );
   }
