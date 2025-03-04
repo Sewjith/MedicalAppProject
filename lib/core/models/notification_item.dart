dda110dd25743818b872c6dc410f2a9710b4190c
Sandrea-MichD <sandream456@gmail.com>
2025-03-04 16:19:51 +0530

    NoficationItem

---
+++ lib/core/models/notification_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NotificationItem {
+  final String title;
+  final String description;
+  final String timeAgo;
+  final String dateGroup;
+  final IconData icon;
+  bool isRead;
+
+  NotificationItem({
+    required this.title,
+    required this.description,
+    required this.timeAgo,
+    required this.dateGroup,
+    required this.icon,
+    this.isRead = false,
+  });
+}
