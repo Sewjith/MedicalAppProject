e37b4f6e17e39cb0237077bf143a4303b4164a9f
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-03-21 13:50:54 +0530

    Updated the body section of the page

--- lib/features/medication_reminder.dart
+++ lib/features/medication_reminder.dart
@@ -22,5 +22,116 @@ class ReminderPage extends StatelessWidget{
     {'time': '18:00', 'name': 'Vitamin C', 'details': '1 pill once per day'},
     {'time': '21:00', 'name': 'Aspirin', 'details': '1 pill once per day'}
   ];
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: AppPallete.headings,
+      body: SafeArea(
+          child: Padding(padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Friday, 23 Nov 2024",
+                style: TextStyle(
+                  color: AppPallete.secondaryColor,
+                  fontSize: 16
+                ),
+              ),
+              SizedBox(height: 6),
+              Text(
+                "Today's plan:",
+                style: TextStyle(
+                  color: AppPallete.whiteColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35
+                ),
+              ),
+              SizedBox(height: 20),
+              Expanded(child: ListView.builder(
+                itemCount: pillPlan.length,
+                itemBuilder: (context, index) {
+                  return PillCard(
+                    time: pillPlan[index]['time']!,
+                    name: pillPlan[index]['name']!,
+                    details: pillPlan[index]['details']!,
+                  );
+                },
+              ),
+              ),
+            ],
+          ),
+          ),
+      ),
+    );
+  }
 }
+class PillCard extends StatelessWidget{
+  final String time;
+  final String name;
+  final String details;
+
+  const PillCard({
+    required this.time,
+    required this.name,
+    required this.details
+});
+  @override
+  Widget build (BuildContext context){
+    return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          time,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: AppPallete.whiteColor
+          ),
+        ),
+        Card(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(padding: const EdgeInsets.all(12),
+          child: Row(
+            children: [
+              Icon(Icons.medication_rounded, color: AppPallete.headings, size: 40,),
+              SizedBox(width: 10),
+              Expanded(child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16
+                    ),
+                  ),
+                  Text(
+                    details,
+                    style: TextStyle(
+                      fontSize: 14, color: Colors.black45
+                    ),
+                  ),
+                ],
+              ),),
+              IconButton(
+                icon: Icon(Icons.arrow_forward_ios_outlined, color: Colors.black45,),
+                onPressed: (){
+                  Navigator.push(context,
+                  MaterialPageRoute(builder:(context) => Placeholder()),);
+                },
+              ),
+            ],
+          )),
+        )
+      ],
+    );
+  }
+}
+
+
 
