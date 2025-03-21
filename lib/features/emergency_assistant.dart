e74d96e110ca6c52696bcc02b9fb0c229bb65c38
Shahani Samsudeen <samsudeengs09@gmail.com>
2025-03-21 10:30:00 +0530

    Added Emergency Assistant feature

---
+++ lib/features/emergency_assistant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:medical_app/core/themes/app_themes.dart';
+import 'package:medical_app/core/themes/color_palette.dart';
