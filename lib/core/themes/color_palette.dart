1ca333bec46abc0330e0ef0b6d3dbd3aeb59b82c
Shahani Samsudeen <samsudeengs09@gmail.com>
2025-03-21 10:34:08 +0530

    Updated

---
+++ lib/core/themes/color_palette.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppPallete {
+  static const Color backgroundColor = Color(0xFFF6F6F6); // Light Gray
+  static const Color primaryColor = Color(0xFF478FE2); // Blue
+  static const Color secondaryColor = Color(0xFFFAFAFA); // Soft White
+  static const Color lightBackground = Color(0xFFF5F5F5); // Off White
+  static const Color darkText = Color(0xFFE8E8E8); // Grayish White
+  static const Color borderColor = Color(0xFFF7F7F7); // Muted Gray
+  static const Color whiteColor = Colors.white;
+  static const Color textColor = Colors.black;
+  static const Color greyColor = Colors.grey;
+  static const Color errorColor = Colors.redAccent;
+  static const Color transparentColor = Colors.transparent;
+  static const Color headings = Colors.blueAccent;
+}
