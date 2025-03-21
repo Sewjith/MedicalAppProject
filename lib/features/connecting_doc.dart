c651463ac58dc01e32748cd762165e2b13795ac1
Shahani Samsudeen <samsudeengs09@gmail.com>
2025-03-21 16:54:51 +0530

    added botom navigation

--- lib/features/connecting_doc.dart
+++ lib/features/connecting_doc.dart
@@ -1,2 +1,40 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+
+
+
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  BottomNavBar({required this.selectedIndex, required this.onItemTapped});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: selectedIndex, // Current selected index
+      onTap: onItemTapped, // Handle item tap
+      selectedItemColor: Colors.blue, // Change color when selected
+      unselectedItemColor: Colors.grey[700], // Change color when unselected
+      type: BottomNavigationBarType.fixed, // Fixed navigation bar, no animation
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "", // Empty label (icon only)
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
+          label: "", // Empty label (icon only)
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: "", // Empty label (icon only)
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          label: "", // Empty label (icon only)
+        ),
+      ],
+    );
+  }
+}
