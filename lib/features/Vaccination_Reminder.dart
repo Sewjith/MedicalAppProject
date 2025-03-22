505af868a1434bea22bdc9be240fca0f309b0795
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-03-22 06:45:15 +0530

    Updated the body part of the page

--- lib/features/Vaccination_Reminder.dart
+++ lib/features/Vaccination_Reminder.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:medical_app/core/themes/color_palette.dart';
 import 'package:medical_app/core/bottom_nav.dart';
+import 'package:medical_app/features/medication_reminder.dart';
 
 void main(){
   runApp(Vaccination_Reminder());
@@ -25,8 +26,112 @@ class Vaccination extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppPallete.headings,
+      backgroundColor: AppPallete.whiteColor,
+        appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppPallete.transparentColor,
+        leading: IconButton(
+        icon: Icon(Icons.arrow_back_ios_new_sharp, color: AppPallete.headings),
+    onPressed: () {
+    Navigator.pushReplacement(
+    context, MaterialPageRoute(builder: (context) => ReminderPage()));
+    },
+    ),
+        title:
+        Text(
+          "Today's Plan",
+          style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: AppPallete.headings
+          ),
+        ),
+          centerTitle: true,
+        ),
+    body: SafeArea(
+    child: Padding(padding: const EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+      'Friday, 23 Nov 2024',
+      style: TextStyle(
+    color: AppPallete.textColor
+    ),
+      ),
+      SizedBox(height: 20),
+      Expanded(child: ListView.builder(
+        itemCount: vaccinePlan.length,
+        itemBuilder: (context, index){
+          return vaccineCard(
+            name: vaccinePlan[index]['name']!,
+            details: vaccinePlan[index]['details']!
+          );
+        },
+      ),
+      ),
+    ],
+    ),
+    ),
+    ),
+    );
+  }
+}
+class vaccineCard extends StatelessWidget{
+  final String name;
+  final String details;
 
+  const vaccineCard({
+    required this.name,
+    required this.details
+});
+  @override
+  Widget build (BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Card(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(padding: const EdgeInsets.all(12),
+              child: Row(
+                children: [
+                  Icon(Icons.medication_rounded, color: AppPallete.headings,
+                    size: 40,),
+                  SizedBox(width: 10),
+                  Expanded(child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                        ),
+                      ),
+                      Text(
+                        details,
+                        style: TextStyle(
+                            fontSize: 14, color: Colors.black45
+                        ),
+                      ),
+                    ],
+                  ),),
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_forward_ios_outlined, color: Colors.black45,),
+                    onPressed: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(
+                            builder: (context) => Placeholder()),);
+                    },
+                  ),
+                ],
+              )),
+        )
+      ],
     );
   }
 }
