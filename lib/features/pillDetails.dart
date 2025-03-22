4ce12fb260defd27d81d6d84913c1152c842ae4e
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-03-22 07:29:21 +0530

    Added pillDetails page

---
+++ lib/features/pillDetails.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:medical_app/core/themes/color_palette.dart';
+import 'package:medical_app/core/bottom_nav.dart';
+import 'package:medical_app/features/medication_reminder.dart';
+
+class pillDetails extends StatelessWidget{
+  final String name;
+  final String details;
+
+  pillDetails({required this.name, required this.details});
+
+  @override
+  Widget build(BuildContext context){
+
+  }
+
+}
