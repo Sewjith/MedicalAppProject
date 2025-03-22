7c8311f492bb41fd59a4c5d8b96b0bd9b5516cb4
Shahani Samsudeen <samsudeengs09@gmail.com>
2025-03-22 19:37:36 +0530

    updated main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medical_app/features/emergency_assistant.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,15 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            height: 200,
-            color: const Color.fromARGB(255, 118, 218, 232),
-            child: Center(child: Text("Demo!"),),
-          ),
-        ),
-      ),
+      home:EmergencyAssistantPage()
     );
     
   }
