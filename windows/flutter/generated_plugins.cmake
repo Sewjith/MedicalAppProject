ecc77f2f078e344876ef21d93b1b20c758bfbac3
Sewjith <sewjithsilva@users.noreply.github.com>
2025-03-26 12:04:22 +0530

    Update Pubspec.lock

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,11 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  app_links
-  url_launcher_windows
   agora_rtc_engine
+  app_links
   iris_method_channel
   permission_handler_windows
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
