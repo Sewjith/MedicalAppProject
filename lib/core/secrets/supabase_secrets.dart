de75cc3affd32274fc33598763387f7aafd679f3
anusarawick <anusarawick@gmail.com>
2025-03-26 16:23:37 +0530

    update supabase key

--- lib/core/secrets/supabase_secrets.dart
+++ lib/core/secrets/supabase_secrets.dart
@@ -1,4 +1,5 @@
 class SupabaseSecrets {
-  static const supabaseUrl = "https://qgjnnocobueozglvqywc.supabase.co";
-  static const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnam5ub2NvYnVlb3pnbHZxeXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2Mzk1NzYsImV4cCI6MjA1NTIxNTU3Nn0.E0C0E1c3hZ6DU1Yn8Zwxp81auO1hwzc4kxfXLHebQQM";
+  static const supabaseUrl = "https://ipoouxwecvbxvsfomfaf.supabase.co";
+  static const key =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb291eHdlY3ZieHZzZm9tZmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDAxMjQsImV4cCI6MjA1NzcxNjEyNH0.lXIIiGsmaiWLPvJhrAuwPjD_r_vcBfkGS0zaGtvuswI";
 }
