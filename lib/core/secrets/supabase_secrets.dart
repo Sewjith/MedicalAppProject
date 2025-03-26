3a5861bc2e9dc4b9845cee800488e16151c744a2
Thimira-Hansana <148532453+Thimira-Hansana@users.noreply.github.com>
2025-03-26 17:58:07 +0530

    Backend update
    Updated the Doctor Availability backend

--- lib/core/secrets/supabase_secrets.dart
+++ lib/core/secrets/supabase_secrets.dart
@@ -1,4 +1,5 @@
 class SupabaseSecrets {
-  static const supabaseUrl = "";
-  static const key = "";
+  static const supabaseUrl = "https://ipoouxwecvbxvsfomfaf.supabase.co";
+  static const key =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb291eHdlY3ZieHZzZm9tZmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDAxMjQsImV4cCI6MjA1NzcxNjEyNH0.lXIIiGsmaiWLPvJhrAuwPjD_r_vcBfkGS0zaGtvuswI";
 }
