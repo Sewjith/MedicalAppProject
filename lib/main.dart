c94329f8d30dc4265933d65dfc077077c842ae46
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-04-05 21:40:22 +0530

    Connected to the backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:medical_app/core/router.dart';
 import 'package:medical_app/core/themes/app_themes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://ipoouxwecvbxvsfomfaf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb291eHdlY3ZieHZzZm9tZmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDAxMjQsImV4cCI6MjA1NzcxNjEyNH0.lXIIiGsmaiWLPvJhrAuwPjD_r_vcBfkGS0zaGtvuswI',
+
+  );
+  Supabase.instance.client.storage.setAuth(
+      Supabase.instance.client.auth.currentSession?.accessToken ?? ''
+  );
+
   runApp(MyApp());
 }
 
