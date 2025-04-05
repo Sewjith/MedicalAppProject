7e111297bd26c6bf69ccd18e66d50d71b8eba8e7
Shahani Samsudeen <samsudeengs09@gmail.com>
2025-04-05 20:03:05 +0530

    updated main dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:medical_app/core/router.dart';
 import 'package:medical_app/core/themes/app_themes.dart';
 import 'package:medical_app/features/doctor_dahboard/firstpage.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://ipoouxwecvbxvsfomfaf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb291eHdlY3ZieHZzZm9tZmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDAxMjQsImV4cCI6MjA1NzcxNjEyNH0.lXIIiGsmaiWLPvJhrAuwPjD_r_vcBfkGS0zaGtvuswI',
+  );
+
   runApp(MyApp());
 }
+
 
 class MyApp extends StatefulWidget {
   @override
