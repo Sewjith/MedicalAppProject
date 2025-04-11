582ec2332d0043f550815b455f6095ae71d0e60c
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-04-11 18:36:28 +0530

    Connected to the backend

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:medical_app/core/router.dart';
 import 'package:medical_app/core/themes/app_themes.dart';
 import 'package:medical_app/features/patient_dashboard/dashboard.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://ipoouxwecvbxvsfomfaf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb291eHdlY3ZieHZzZm9tZmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDAxMjQsImV4cCI6MjA1NzcxNjEyNH0.lXIIiGsmaiWLPvJhrAuwPjD_r_vcBfkGS0zaGtvuswI',
+
+  );
+  Supabase.instance.client.storage.setAuth(
+      Supabase.instance.client.auth.currentSession?.accessToken ?? ''
+  );
+
   runApp(MyApp());
 }
 
@@ -15,10 +27,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: Dashboard(),
-
+      routerConfig: appRouter,
       title: 'Medical App',
       theme: AppTheme.lightThemeMode,
     );
