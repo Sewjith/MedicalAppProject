4affe03328a34ef3c5dd62cb539cc3391b54ce54
Sandrea-MichD <sandream456@gmail.com>
2025-04-14 11:05:58 +0530

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,18 @@ import 'package:medical_app/core/router.dart';
 import 'package:medical_app/core/supabase_config.dart';
 import 'package:medical_app/core/themes/app_themes.dart';
 import 'package:medical_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://ipoouxwecvbxvsfomfaf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb291eHdlY3ZieHZzZm9tZmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDAxMjQsImV4cCI6MjA1NzcxNjEyNH0.lXIIiGsmaiWLPvJhrAuwPjD_r_vcBfkGS0zaGtvuswI',
+  );
+
+  runApp(MyApp());
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
