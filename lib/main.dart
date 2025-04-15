d190110fb0d41d90bac7d859b85cce8c202f7076
Sandani Hettiarachchi <hettiarachchisandani@gmail.com>
2025-04-15 17:03:42 +0530

    Connected to the backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:medical_app/features/Vaccination_Reminder.dart';
 import 'package:medical_app/features/medication_reminder.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://ipoouxwecvbxvsfomfaf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb291eHdlY3ZieHZzZm9tZmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDAxMjQsImV4cCI6MjA1NzcxNjEyNH0.lXIIiGsmaiWLPvJhrAuwPjD_r_vcBfkGS0zaGtvuswI',
+  );
+
+  Supabase.instance.client.storage.setAuth(
+    Supabase.instance.client.auth.currentSession?.accessToken ?? '',
+  );
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ReminderPage()
+      title: 'Medical App',
+      home: Medication_Reminder(),
     );
-    
   }
 }
