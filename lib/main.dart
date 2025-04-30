f37140b3d0d7c057144f769860dfd5369f7f10c2
Devanitha-sde <sathkunasundaramdevanitha@gmail.com>
2025-04-30 14:46:43 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:medical_app/core/router.dart';
 import 'package:medical_app/core/themes/app_themes.dart';
 
