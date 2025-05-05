adc6f07914d7725d78ef71e72f20d97d926e81aa
Sewjith <185014316+Sewjith@users.noreply.github.com>
2025-05-05 21:01:23 +0530

    Deleted the secrets
    Deleted the secrets

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://ipoouxwecvbxvsfomfaf.supabase.co',
+    url: '',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb291eHdlY3ZieHZzZm9tZmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDAxMjQsImV4cCI6MjA1NzcxNjEyNH0.lXIIiGsmaiWLPvJhrAuwPjD_r_vcBfkGS0zaGtvuswI',
+        '',
   );
 
   runApp(const MyApp());
