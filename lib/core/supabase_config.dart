05c3ee437fd8b72662f1f72492ece2070ef6328a
Sewjith <185014316+Sewjith@users.noreply.github.com>
2025-05-05 20:29:59 +0530

    Deleted the secrets
    Deleted the secrets

--- lib/core/supabase_config.dart
+++ lib/core/supabase_config.dart
@@ -5,8 +5,8 @@ class SupabaseConfig {
 
   static Future<void> initialize() async {
     await Supabase.initialize(
-      url:  "https://qgjnnocobueozglvqywc.supabase.co", // Replace with actual URL
-      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnam5ub2NvYnVlb3pnbHZxeXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2Mzk1NzYsImV4cCI6MjA1NTIxNTU3Nn0.E0C0E1c3hZ6DU1Yn8Zwxp81auO1hwzc4kxfXLHebQQM", // Replace with actual Anon Key
+      url:  "", // Replace with actual URL
+      anonKey: "", // Replace with actual Anon Key
     );
     _supabase = Supabase.instance.client;
   }
