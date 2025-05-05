fd69b47f6f0ebdb1bd46a9751d6bc589cd63b16f
Sewjith <185014316+Sewjith@users.noreply.github.com>
2025-05-05 21:00:14 +0530

    Deleted the secrets
    Deleted the secrets

--- lib/core/secrets/supabase_secrets.dart
+++ lib/core/secrets/supabase_secrets.dart
@@ -1,4 +1,4 @@
 class SupabaseSecrets {
-  static const supabaseUrl = "https://qgjnnocobueozglvqywc.supabase.co";
-  static const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnam5ub2NvYnVlb3pnbHZxeXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2Mzk1NzYsImV4cCI6MjA1NTIxNTU3Nn0.E0C0E1c3hZ6DU1Yn8Zwxp81auO1hwzc4kxfXLHebQQM";
+  static const supabaseUrl = "";
+  static const key = "";
 }
